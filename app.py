4c094c67f2bc89ef377716b4fc6a508dc4d8b2ab
ALOHAHAWA11 <una@itob.ru>
2023-09-03 15:08:45 +0400

    add logging and db handler

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, render_template, jsonify
+from db import DataBaseConnection
+from loguru import logger
+from datetime import datetime
 
 app = Flask(__name__)
+db_handler = DataBaseConnection()
+logger.add(f"logs/{datetime.now().date()}.log", rotation="500 MB")
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    ds = {
+        'count': db_handler.make_select_query(
+                    "SELECT count FROM COUNTER"
+                )[0][0]
+    }
+    logger.info('Render index template')
+    return render_template('index.html', ds=ds)
 
 @app.route("/send_request", methods=["GET"])
 def accept_request():
+    db_handler.make_update_query(
+        "UPDATE counter SET count = count + 1"
+    )
     ds = {
         "msg": "It`s time to increase your counter!",
         "status_code": 200
     }
+    logger.info("Increase counter")
     return jsonify(ds)
 
 
 if __name__ == '__main__':
+    logger.info("Start app")
     app.run(debug=True, host='0.0.0.0', port=5000)
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,7 +1,7 @@
 function sendRequest() {
     const xhr = new XMLHttpRequest();
     let counter = document.getElementById('requestCounter');
-    xhr.open("GET", "http://0.0.0.0:5000/send_request");
+    xhr.open("GET", "http://127.0.0.1:5000/send_request");
     xhr.onload = () => {
         response = JSON.parse(xhr.responseText);
         if (response['status_code'] == 200) {
